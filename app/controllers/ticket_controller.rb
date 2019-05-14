45bf7fa53ff8db626b7d7ee0415e68846d574f69
Raúl <ribarahonaa@gmail.com>
2019-05-14 14:47:31 -0400

    fixed

--- app/controllers/ticket_controller.rb
+++ app/controllers/ticket_controller.rb
@@ -3,6 +3,7 @@ class TicketController < ApplicationController
     def encolar
         @ticket = Ticket.create()
         #Una vez creado el ticket se procede a lo siguiente:
+        if @ticket.save
             #Define el estado del ticket
             @estado = TicketEstado.new
             @estado.estados_id = 1 #Encolado
@@ -14,6 +15,7 @@ class TicketController < ApplicationController
             @operacion.atraccions_id = params[:id]
             @operacion.tickets_id = @ticket.id
             @operacion.save
+        end
         redirect_to root_path
     end
 
