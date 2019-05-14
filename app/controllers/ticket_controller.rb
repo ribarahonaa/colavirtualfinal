afa96352a51838a1918e1c876d8052c6bdb118b3
Raúl <ribarahonaa@gmail.com>
2019-05-14 14:53:59 -0400

    fix ticket

--- app/controllers/ticket_controller.rb
+++ app/controllers/ticket_controller.rb
@@ -1,22 +1,26 @@
 class TicketController < ApplicationController
 
     def encolar
-        @ticket = Ticket.create()
-        #Una vez creado el ticket se procede a lo siguiente:
-        if @ticket.save
-            #Define el estado del ticket
-            @estado = TicketEstado.new
-            @estado.estados_id = 1 #Encolado
-            @estado.tickets_id = @ticket.id
-            @estado.save
-          #Define la atraccion y el usuario del ticket
-            @operacion = OperacionesAtraccion.new
-            @operacion.users_id = current_user[:id]
-            @operacion.atraccions_id = params[:id]
-            @operacion.tickets_id = @ticket.id
-            @operacion.save
+        if current_user
+            @ticket = Ticket.create()
+            #Una vez creado el ticket se procede a lo siguiente:
+            if @ticket.save
+                #Define el estado del ticket
+                @estado = TicketEstado.new
+                @estado.estados_id = 1 #Encolado
+                @estado.tickets_id = @ticket.id
+                @estado.save
+            #Define la atraccion y el usuario del ticket
+                @operacion = OperacionesAtraccion.new
+                @operacion.users_id = current_user[:id]
+                @operacion.atraccions_id = params[:id]
+                @operacion.tickets_id = @ticket.id
+                @operacion.save
+            end
+            redirect_to root_path
+        else
+            redirect_to user_session_path
         end
-        redirect_to root_path
     end
 
 end
