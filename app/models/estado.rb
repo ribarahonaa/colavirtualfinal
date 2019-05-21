32a257cd07cb29778b7a07ae06a0e71ef990d17a
Raúl <ribarahonaa@gmail.com>
2019-05-21 18:31:25 -0400

    pruebas

--- app/models/estado.rb
+++ app/models/estado.rb
@@ -1,4 +1,2 @@
 class Estado < ApplicationRecord
-    # has_many :ticket_estados
-    # has_many :tickets, :through => :ticket_estados
 end
--- app/models/ticket_estado.rb
+++ app/models/ticket_estado.rb
@@ -1,4 +1,2 @@
 class TicketEstado < ApplicationRecord
-    # belongs_to :tickets
-    # belongs_to :estados
 end
