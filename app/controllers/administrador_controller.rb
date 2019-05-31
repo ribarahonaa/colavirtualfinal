247eedc987e9b87314c60e0cf1a8c36ad6cee0de
Raúl <ribarahonaa@gmail.com>
2019-05-30 20:19:28 -0400

    Asignar roles con el super usuario

--- app/controllers/administrador_controller.rb
+++ app/controllers/administrador_controller.rb
@@ -1,17 +1,22 @@
 class AdministradorController < ApplicationController
 
+    # Muestra a todos los usuarios
     def editusers
         @user = User.all
     end
 
+    # Muestra al usuario seleccionado
     def edit
         @user = User.find(params[:id])
         @group = Group.where("id != 1")
     end
 
+    # Asigna rol al usuario
     def asignarrol
         @user = User.find(params[:usuario])
         Group.find(params[:grupo]).add @user
+
+        redirect_to administrador_editusers_path
     end
 
 end
