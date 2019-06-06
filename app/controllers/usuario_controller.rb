b60fd9bf0cdbdaf0bf2e684751dda0b4960e231b
Pablo Díaz <cloud.aggresor@gmail.com>
2019-06-06 18:44:48 -0400

    marcadores Mapa

--- app/controllers/usuario_controller.rb
+++ app/controllers/usuario_controller.rb
@@ -1,2 +1,17 @@
 class UsuarioController < ApplicationController
+
+    def load_atraccions  
+        @atraccions_default = Gmaps4rails.build_markers(Atraccion.all) do |plot, marker|  
+           marker.lat plot.latitude  
+           marker.lng plot.longitude  
+           marker.infowindow render_to_string(:locals => {:nombre_atc => plot.nombre_atc})  
+        end  
+      end
+     
+     def map  
+        load_atraccions  
+     
+        @atraccions = Atraccion.all  
+     end
+
 end
