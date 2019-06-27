bc89bd22b19215e0680d756a3a41cc9d98d536ba
Raúl <ribarahonaa@gmail.com>
2019-06-27 11:33:03 -0400

    mailer produccion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Desarrollo
+  config.action_mailer.default_options = {from: 'contacto.fantasirow@gmail.com'}
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'contacto.fantasirow@gmail.com',
+  password:             'fantasirow0207',
+  authentication:       'plain' 
+  }
+
+  # HOST
+  config.action_mailer.default_url_options = { host: 'fantasyrow.herokuapp.com' }
 end
